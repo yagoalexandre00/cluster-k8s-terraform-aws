c8fc8cd0f045a8776a07423d3874d4fd15795a27
Yago Alexandre <goltarayago@gmail.com>
2023-10-27 12:58:08 -0300

    fix: instance_type: t2.micro

--- kube-nginx/main.tf
+++ kube-nginx/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "kube-nginx" {
   ami           = "ami-0af6e9042ea5a4e3e"
-  instance_type = "t2.mi  cro"
+  instance_type = "t2.micro"
   
   tags = {
     "Name" : "kube-nginx"
